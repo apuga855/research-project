93c5a4dc8d4c0763a1ef8940701cf91819c93c46
apuga855 <alfonsopuga@live.com>
2014-04-20 14:35:49 -0700

    changed algo slightly

--- packetReasssemblyAlgo.c
+++ packetReasssemblyAlgo.c
@@ -7,8 +7,15 @@ typedef struct _dummyPack
    uint16_t derpFragOff;
 }dummyPack;
 
-char* reassemblyAlg(Llist* list, uint16_t size)
+char* reassemblyAlg(Llist* list, uint16_t size, FILE  file)
 {
+   fprintf(file, "Inside of ReassemblyAlg\n");
+   if(LlistIsEmpty(list))
+   {
+      fprintf(file, "The list was empty, exiting with an error\n");
+      return NULL;
+   }
+
    uint16_t runningCount = size;
    char* dataReady = malloc(sizeof(char) * size);
    uint16_t fragOff = 0;
