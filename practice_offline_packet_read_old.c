698814d93afbd0480c97abf5f583ee99ebc81bc2
Gabe Pike <gpike75@gmail.com>
2014-05-17 22:37:32 -0700

    print pretty ip addresses

--- practice_offline_packet_read_old.c
+++ practice_offline_packet_read_old.c
@@ -19,6 +19,7 @@
 #define ipLen  sizeof(struct ip)
 #define tcpLen  sizeof(struct tcphdr)
 #define fngPntLen 13
+#define INET4_ADDRSTRLEN 16
 
 //starting from here, is all necessary materials and tools for ---------------------
 //packet capture without reasembly
@@ -340,10 +341,23 @@ void packet_handler(u_char* usrData,const struct pcap_pkthdr* pcktHeader, const 
 
 //printing function
 
+
+
 void SRDPcktPrint(sensorRdyPckt *curPckt)
 {
-   printf("Printing packet #%u\nSource Address: %d\nDestination Address: %d\nSource Port: %d\nDestination Port: %d\n" 
-	  ,curPckt->SRD_id, curPckt->SRD_src_addr, curPckt->SRD_dst_addr, curPckt->SRD_src_prt, curPckt->SRD_dst_prt);
+    char src_addr[INET4_ADDRSTRLEN], dst_addr[INET4_ADDRSTRLEN];
+    struct in_addr s, d;
+    inet_ntop(AF_INET, &s, src_addr, sizeof(src_addr));
+    inet_ntop(AF_INET, &d, dst_addr, sizeof(dst_addr));
+
+    printf("Printing packet #%u\n"
+          "Source Address: %s\n"
+          "Destination Address: %s\n"
+          "Source Port: %d\n"
+          "Destination Port: %d\n",
+          curPckt->SRD_id,
+          src_addr, dst_addr,
+          curPckt->SRD_src_prt, curPckt->SRD_dst_prt);
 }
 
 
